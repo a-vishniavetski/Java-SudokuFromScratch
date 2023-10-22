5a324fd98b9d4ae154aba82442e8d10cfb08422b
240343 Vishniavetski Aliaksei <240343 Vishniavetski Aliaksei>
2023-10-22 14:47:59 +0200

    Zadanie2 naprawione

---
+++ Zadanie2/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        SudokuBoard board = new SudokuBoard();
+        board.fillBoard();
+        board.printBoard();
+
+    }
+}
